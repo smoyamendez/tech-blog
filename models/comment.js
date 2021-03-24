6ce461627db7a5abd68aca2b020f678a888e0416
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-23 22:21:18 -0400

    comment model written

---
+++ models/comment.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment; 
--- models/post.js
+++ models/post.js
@@ -19,7 +19,7 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false,
         },
-        date: {
+        post_date: {
             type: DataTypes.DATE,
             allowNull: false,
         },
