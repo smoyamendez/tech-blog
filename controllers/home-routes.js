aa72b474bafd7bb7b7af1a4fe6a0767e80b88cc2
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-23 22:54:33 -0400

    user-routes and api index written

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+// TODO: SEE ACT 16
+
+const router = require('express').Router();
+// TODO: ADD MODELS NEEDED
+const {} = require('../models');
+
+// /dashboard
+
+// /signup
+
+// GET /login
+
+
+
+module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,1 @@
+// TODO: SEE ACT 16 
